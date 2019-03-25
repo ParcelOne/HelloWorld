6c3ccbf33bbc94ed187987be538eba60ae77aa1e
softimal <js@softimal.de>
2019-03-25 13:41:37 +0100

    Update WsseAuthHeader.php

--- src/API/WsseAuthHeader.php
+++ src/API/WsseAuthHeader.php
@@ -24,7 +24,8 @@ use  HelloWorld\API\SoapHeaderWrapper;
         'Password' => pluginApp(SoapVarWrapper::class, [$pass, XSD_STRING, "string", $this->wss_ns, NULL, $this->wss_ns])
       );
 
-      $UsernameToken = pluginApp(UsernameToken::class,[ pluginApp(SoapVarWrapper::class, [$auth, SOAP_ENC_OBJECT, "string", $this->wss_ns, 'UsernameToken', $this->wss_ns]) ]);
+      //$UsernameToken = pluginApp(UsernameToken::class,[ pluginApp(SoapVarWrapper::class, [$auth, SOAP_ENC_OBJECT, "string", $this->wss_ns, 'UsernameToken', $this->wss_ns]) ]);
+      $UsernameToken = pluginApp(UsernameToken::class,[ 'hallo' ]);
 
       $security_sv = pluginApp(SoapVarWrapper::class, [
         pluginApp(SoapVarWrapper::class, [$UsernameToken, SOAP_ENC_OBJECT, NULL, $this->wss_ns, 'UsernameToken', $this->wss_ns]),
