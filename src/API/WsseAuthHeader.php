d48c32e0b30900539b3a9b3e6564ac41923cbc45
softimal <js@softimal.de>
2019-03-25 15:53:32 +0100

    newtest

--- src/API/WsseAuthHeader.php
+++ src/API/WsseAuthHeader.php
@@ -12,24 +12,27 @@ use  HelloWorld\API\SoapHeaderWrapper;
  * Possibility to access SoapHeader over pluginApp()
  *
  * Class WsseAuthHeader
- * @package HelloWorld\API
+ * @package ParcelOneShipping\API
  */
  class WsseAuthHeader extends \SoapHeader {
 	private $wss_ns = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd';
 	private $wsu_ns = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd';
 	public function __construct($user, $pass, $nspace) {
+      $auth = pluginApp(standardClass::class,[]);
+      $auth = (object)array(
+        'Username' => $user,
+        'Password' => $pass
+      );
 
-    $xml = '
-<wsse:Security SOAP-ENV:mustUnderstand="1" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
-   <wsse:UsernameToken>
-       <wsse:Username>'.$user.'</wsse:Username>
-       <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">'.$pass.'</wsse:Password>
-   </wsse:UsernameToken>
-</wsse:Security>
-';
+      $username_token = pluginApp(standardClass::class,[]);
 
-      //$xml2Return = pluginApp(SoapVarWrapper::class, [$xml, XSD_ANYXML, NULL, NULL, NULL, NULL]);
-	    parent::__construct($this->wss_ns, 'Security', $xml, true);
+      $username_token = (object)array(
+        'UsernameToken xmlns="'.$this->wss_ns.'"' => $auth
+      );
+
+      $security_sv = pluginApp(standardClass::class,[]);
+      $security_sv->Security = $username_token;
+	    parent::__construct($this->wss_ns, 'Security', $username_token, false);
 	}
 }
 ?>
---
+++ src/API/WsseAuthHeader1.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace HelloWorld\API;
+use SoapVar;
+use  HelloWorld\API\UsernameToken;
+use  HelloWorld\API\AuthentificationType;
+
+use  HelloWorld\API\standardClass;
+use  HelloWorld\API\SoapVarWrapper;
+use  HelloWorld\API\SoapHeaderWrapper;
+/**
+ * Possibility to access SoapHeader over pluginApp()
+ *
+ * Class WsseAuthHeader
+ * @package HelloWorld\API
+ */
+ class WsseAuthHeader1 extends \SoapHeader {
+	private $wss_ns = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd';
+	private $wsu_ns = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd';
+	public function __construct($user, $pass, $nspace) {
+
+    $xml = '
+<wsse:Security SOAP-ENV:mustUnderstand="1" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
+   <wsse:UsernameToken>
+       <wsse:Username>'.$user.'</wsse:Username>
+       <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">'.$pass.'</wsse:Password>
+   </wsse:UsernameToken>
+</wsse:Security>
+';
+
+      //$xml2Return = pluginApp(SoapVarWrapper::class, [$xml, XSD_ANYXML, NULL, NULL, NULL, NULL]);
+	    parent::__construct($this->wss_ns, 'Security', $xml, true);
+	}
+}
+?>
